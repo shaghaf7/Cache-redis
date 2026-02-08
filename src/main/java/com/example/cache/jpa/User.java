3b7f2c66dbcf7c3a82313a58fef759cc02455f19
shaghaf7 <shaghafkashif@gmail.com>
2026-02-08 18:13:36 +0530

    added the main file

---
+++ src/main/java/com/example/cache/jpa/User.java
@@ -0,0 +1,26 @@
+package com.example.cache.jpa;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    private String role;
+}
