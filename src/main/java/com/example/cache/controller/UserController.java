96a1e943d71825ee3992d135ee65ee3015a85b06
shaghaf7 <shaghafkashif@gmail.com>
2026-02-13 22:31:16 +0530

    added controller for login

--- src/main/java/com/example/cache/controller/UserController.java
+++ src/main/java/com/example/cache/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.example.cache.controller;
 
+import com.example.cache.Service.UserService;
+import com.example.cache.jpa.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private  UserService userService;
+
+    @PostMapping
+    public User create(@RequestBody User user) {
+        return userService.save(user);
+    }
+
+    @GetMapping("/{username}")
+    public User getByUsername(@PathVariable String username) {
+        return userService.getByUsername(username);
+    }
 }
