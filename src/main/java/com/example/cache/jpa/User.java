a54638534dd8466576106de131e1147f956c4c06
shaghaf7 <shaghafkashif@gmail.com>
2026-02-13 22:31:38 +0530

    .

--- src/main/java/com/example/cache/jpa/User.java
+++ src/main/java/com/example/cache/jpa/User.java
@@ -19,6 +19,38 @@ public class User {
     @Column(unique = true, nullable = false)
     private String username;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
     @Column(nullable = false)
     private String password;
 
