48dc072098aab9922001884603ec99c51d531e6b
shaghaf7 <shaghafkashif@gmail.com>
2026-02-13 22:30:12 +0530

    added userservice

--- src/main/java/com/example/cache/Service/UserService.java
+++ src/main/java/com/example/cache/Service/UserService.java
@@ -1,4 +1,25 @@
 package com.example.cache.Service;
 
+import com.example.cache.jpa.User;
+import com.example.cache.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private  UserRepository userRepository;
+
+    @Cacheable(value = "users", key = "#username")
+    public User getByUsername(String username) {
+        System.out.println("Fetching from DB...");
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
